5d16b80f4c66ba454d89ebf26b4cf5e8b0b6c22c
oscar2246 <oscarhl2367@gmail.com>
2026-06-14 14:58:41 -0400

    Externalize AD config to gitignored secrets_local

--- AD_operations.py
+++ AD_operations.py
@@ -1,15 +1,15 @@
 from ldap3 import Server, Connection
-from secrets_local import SVC_PW
+import secrets_local
 
 def initialize_connection():
 
 
-    server = Server("ldap://10.0.0.143")
+    server = Server(f"ldap://{secrets_local.DC_host_ip}")
 
     conn = Connection(
         server,
-        user="svc_iamcli@idm.internal",
-        password=SVC_PW,
+        user=secrets_local.SVC_Account_AD,
+        password=secrets_local.SVC_PW,
         auto_bind=True,   
     )
 
@@ -21,7 +21,7 @@ def search_user_AD(conn, sAMAccountName_search):
      
      
      conn.search(
-                'OU=TestUsers,OU=IDM_Lab,DC=idm,DC=internal', 
+                secrets_local.SEARCH_BASE, 
                     f'(&(ObjectClass=user)(objectCategory=Person)(sAMAccountName={sAMAccountName_search}))', 
                     attributes=['sAMAccountName', 'mail'],
     )
