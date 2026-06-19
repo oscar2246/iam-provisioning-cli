73fc69aab476755b2b78833388c348c5b046ff1e
oscar2246 <oscarhl2367@gmail.com>
2026-06-18 21:50:04 -0400

    Fixed user class implementation

--- user_ops.py
+++ user_ops.py
@@ -36,13 +36,16 @@ def gather_information():
     
 class user_creation:
     
-    def __init__(self, f_name, m_name, l_name):
+    def __init__(self, f_name, m_name, l_name, existing_lan_ids, existing_emails):
             self.f_name = f_name
             self.m_name = m_name
             self.l_name = l_name
+            self.user_id = str(uuid4())
+            self.account_status = "disabled"
+            self.set_lan(existing_lan_ids)
+            self.set_email(existing_emails)
 
     def set_lan(self, existing_lan_ids):
-        #Gathering existing Emails & LANID
         
         #LANID generation
         base_lan_id = (f"{self.f_name[:4]}{self.m_name[:1]}{self.l_name[:2]}").upper()
@@ -51,7 +54,8 @@ class user_creation:
         while LANID in existing_lan_ids:
             LANID = f"{base_lan_id}{l_counter}"
             l_counter += 1
-        return LANID
+        self.LANID = LANID
+    
     def set_email(self, existing_emails):
         #Email generation
         base_email_local = f"{self.f_name}.{self.l_name}"
@@ -63,7 +67,7 @@ class user_creation:
             #if John.doe@example.internal exists then it will use john.doe1@example.com, avoiding collision
             email = f"{base_email_local}{e_counter}@{domain}".lower()
             e_counter += 1
-        return email
+        self.email = email
 
 
 def create_user():
@@ -74,42 +78,36 @@ def create_user():
         print("Information comes back as None")
         return
 
-
-    user_id = str(uuid4())
-
     f_name = information[0]
     m_name = information[1]
     l_name = information[2]
     existing_lan_ids = information[3]
     existing_emails = information[4]
 
-    new_user_obj = user_creation(f_name, m_name, l_name)
-
-    LANID = new_user_obj.set_lan(existing_lan_ids)
-    email = new_user_obj.set_email(existing_emails)
+    new_user_obj = user_creation(f_name, m_name, l_name, existing_lan_ids, existing_emails)
     
     #grouping all new user information
     new_user = {
         "Last Name": l_name,
         "First Name": f_name,
         "Middle Name": m_name,
-        "Email": email,
-        "LANID": LANID,
-        "user_id": user_id,
-        "Account Status": "disabled"
+        "Email": new_user_obj.email,
+        "LANID": new_user_obj.LANID,
+        "user_id": new_user_obj.user_id,
+        "Account Status": new_user_obj.account_status
     }
     
     users.append(new_user)
     storage.save_users(users)
-    storage.log_event("CREATED", LANID)
+    storage.log_event("CREATED", new_user_obj.LANID)
 
     m_initial = m_name[0] if m_name else ""
     
     print(f"\nSuccessfully created account for {l_name}, {f_name} {m_initial}")
-    print(f"Email: {email}")
-    print(f"LANID: {LANID}")
-    print(f"User ID: {user_id}")
-    print("Account Status: disabled")
+    print(f"Email: {new_user_obj.email}")
+    print(f"LANID: {new_user_obj.LANID}")
+    print(f"User ID: {new_user_obj.user_id}")
+    print(f"Account Status: {new_user_obj.account_status}")
 
 def list_users():
     print(" --- Users --- ")
